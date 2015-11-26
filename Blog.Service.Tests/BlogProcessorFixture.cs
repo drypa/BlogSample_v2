4a27fea31fdafcb9a25f66b4f46bb2304972ffea
DRypa <dr.drypa.k@mail.ru>
2015-11-26 09:34:29 +0300

    test on producer

---
+++ Blog.Service.Tests/BlogProcessorFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Blog.BusinessEntities.Contract;
+using Blog.BusinessLogic;
+using Blog.ConsoleService;
+using Moq;
+using Xunit;
+
+namespace Blog.Service.Tests
+{
+    public class BlogProcessorFixture
+    {
+        [Fact]
+        public void BlogServiceShouldReadThrougthReadRepository()
+        {
+            var readRepoMock = new Mock<IBlogRepository>();
+
+            var blogProcessor = new BlogProcessor(readRepoMock.Object, new ExchangeConfigurationProvider().Configuration);
+
+            blogProcessor.Get(new GetPostRequest());
+            readRepoMock.Verify(x => x.GetPost(It.IsAny<Guid>()), Times.Once());
+
+            blogProcessor.Get(new GetPostsRequest());
+            readRepoMock.Verify(x => x.GetPosts(), Times.Once());
+        }
+    }
+}
