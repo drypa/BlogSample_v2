133f0d87c515711c2212b44eff2f6e1f589dd666
DRypa <dr.drypa.k@mail.ru>
2015-11-27 17:06:36 +0300

    correct check post added

--- Blog.FitNesse.Tests/BlogFixture.cs
+++ Blog.FitNesse.Tests/BlogFixture.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Threading;
 using Blog.BusinessEntities;
 using Blog.BusinessLogic.Client;
@@ -8,11 +12,62 @@ namespace Blog.FitNesse.Tests
 {
     public class BlogFixture : DoFixture
     {
+
+
         public void AddPost()
         {
             IBlogClient client = new BlogClient(Commands.ServiceUrl);
-            client.AddPost(new BlogPost { Text = "text", Title = "title" });
-            Thread.Sleep(5000);
+            var postToAdd = new BlogPost { Text = "text", Title = "title" };
+            client.AddPost(postToAdd);
+            WaitUntilPostAdded(postToAdd);
         }
+
+        private void WaitUntilPostAdded(BlogPost post)
+        {
+            while (!GetPosts().Any(x => x.Text == post.Text && x.Title == post.Title))
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+
+        private List<BlogPost> GetPosts()
+        {
+
+            using (var connection = new SqlConnection(Commands.ConnectionString))
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = @"SELECT [Id]
+                                          ,[CreateDate]
+                                          ,[Description]
+                                          ,[Text]
+                                          ,[Title]
+                                      FROM [BlogServiceTest].[dbo].[BlogPost]";
+                    cmd.CommandType = CommandType.Text;
+                    
+                    return Read(cmd.ExecuteReader());
+                }
+            }
+        }
+
+        private List<BlogPost> Read(SqlDataReader reader)
+        {
+            List<BlogPost> posts = new List<BlogPost>();
+            while (reader.Read())
+            {
+                posts.Add(new BlogPost
+                {
+                    CreateDate = (DateTime)reader["CreateDate"],
+                    Id = (Guid)reader["Id"],
+                    Text = reader["Text"].ToString(),
+                    Title = reader["Title"].ToString()
+                });
+                
+            }
+            return posts;
+        }
+
     }
 }
